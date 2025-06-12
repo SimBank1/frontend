fade2a433b328c76bdfa3b91fcef12e487bcf6c1
Maj Mohar <maj.mohar4@gmail.com>
2025-06-12 11:20:40 +0300

    dolzina crk v passwordu

--- src/components/adminPanel/adminPanel.jsx
+++ src/components/adminPanel/adminPanel.jsx
@@ -232,7 +232,7 @@ useEffect(() => {
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     const numbers = "0123456789"
     let password = ""
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 7; i++) {
       password += letters.charAt(Math.floor(Math.random() * letters.length))
     }
     for (let i = 0; i < 3; i++) {
